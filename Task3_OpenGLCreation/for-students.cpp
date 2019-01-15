16de011fd8c14760cdc5e07cb88eeb8d09a6568c
Yeaseen <1405091.ya@ugrad.cse.buet.ac.bd>
2019-01-15 16:19:26 +0600

    Stage1 and Stage2 done

--- Task3_OpenGLCreation/for-students.cpp
+++ Task3_OpenGLCreation/for-students.cpp
@@ -380,10 +380,58 @@ void stage2()
     stage2 << std::fixed;
     stage2 << std::setprecision(7);
 
+    Vector l(look_x-eye_x,look_y-eye_y,look_z-eye_z);
+    l.normalize();
+    Vector Up(up_x,up_y,up_z);
+    Vector r=Vector::cross(l,Up);
+    r.normalize();
+    Vector u=Vector::cross(r,l);
+
+
+    matrix T(4);
+    T=T.make_identity(4);
+    T.values[0][3]=-eye_x;
+    T.values[1][3]=-eye_y;
+    T.values[2][3]=-eye_z;
+
+    matrix viewR(4);
+    viewR=viewR.make_identity(4);
+
+    viewR.values[0][0]=r.x;
+    viewR.values[0][1]=r.y;
+    viewR.values[0][2]=r.z;
+
+    viewR.values[1][0]=u.x;
+    viewR.values[1][1]=u.y;
+    viewR.values[1][2]=u.z;
+
+    viewR.values[2][0]=-l.x;
+    viewR.values[2][1]=-l.y;
+    viewR.values[2][2]=-l.z;
+
+    matrix V=viewR*T;
+    //V.print();
+    homogeneous_point s1p;
+    int cnt=1;
+    while(stage1>> s1p.x >> s1p.y >> s1p.z){
+        s1p.w=1;
+        homogeneous_point viewP=V*s1p;
+        stage2 << viewP.x <<" "<<viewP.y <<" "<<viewP.z<<endl;
+        if((cnt%3)==0) stage2<<endl;
+        cnt++;
+    }
+
+
     // collect input from stage1 and process, write output to stage2
 
     stage1.close();
     stage2.close();
+
+}
+
+Vector R(Vector x, Vector a, double theta){
+    Vector res= x*(cos(theta)) + a*(Vector::dot(a,x)*(1-cos(theta))) + Vector::cross(a,x)*(sin(theta));
+    return res;
 
 }
 
@@ -404,19 +452,102 @@ void stage1()
     scene >> fov_y >> aspectRatio >> near >> far;
     scene >> screen_x >> screen_y;
     scene >> backgroud.r >> backgroud.g >> backgroud.b;
-
+    cout<<screen_x<<endl;
     // take other commands as input from scene in a loop
     // process accordingly
     // write to stage1
-    cout<<"Hello From Stage 1"<<endl;
     matrix Mi(4);
-    matrix identity=Mi.make_identity(4);
-    identity.print();
     stack <matrix> s;
-    s.push(identity);
+    s.push(Mi.make_identity(4));
+    while(true){
+        scene >> command;
+        cout << command<<endl;
+        if(command == "triangle"){
+            for(int i=0;i<3;i++){
+            homogeneous_point pp;
+            scene >> pp.x >>pp.y >>pp.z;
+            pp.w=1.0;
+            //pp.print();
+            //s.top().print();
+            pp=s.top()*pp;
+            //pp.print();
+            stage1 << pp.x <<" "<<pp.y <<" "<<pp.z<<endl;
+        }
+            color trclr;
+            scene >> trclr.r >> trclr.g >> trclr.b;
+            //stage1 << trclr.r<<" " << trclr.g<< " " << trclr.b<<endl;
+            stage1 <<endl;
+        }
+        else if(command == "push"){
+            s.push(s.top());
 
-    cout<<s.size()<<endl;
-    s.top().print();
+        }
+        else if(command == "pop"){
+            s.pop();
+        }
+        else if(command == "rotate"){
+            double angle;
+            Vector rotateAxis(0,0,0);
+            scene >> angle >> rotateAxis.x >> rotateAxis.y >> rotateAxis.z;
+            angle= angle*(pi/180);
+            cout<<angle<<endl;
+            rotateAxis.normalize();
+            rotateAxis.print();
+            Vector i(1,0,0), j(0,1,0), k(0,0,1);
+            Vector c1= R(i, rotateAxis,angle);
+            Vector c2= R(j, rotateAxis, angle);
+            Vector c3= R(k, rotateAxis, angle);
+
+            matrix rotationMat(4);
+
+            rotationMat.values[0][0]=c1.x;
+            rotationMat.values[0][1]=c2.x;
+            rotationMat.values[0][2]=c3.x;
+            rotationMat.values[0][3]=0;
+
+            rotationMat.values[1][0]=c1.y;
+            rotationMat.values[1][1]=c2.y;
+            rotationMat.values[1][2]=c3.y;
+            rotationMat.values[1][3]=0;
+
+            rotationMat.values[2][0]=c1.z;
+            rotationMat.values[2][1]=c2.z;
+            rotationMat.values[2][2]=c3.z;
+            rotationMat.values[2][3]=0;
+
+            rotationMat.values[3][0]=0;
+            rotationMat.values[3][1]=0;
+            rotationMat.values[3][2]=0;
+            rotationMat.values[3][3]=1;
+
+            rotationMat= rotationMat * s.top();
+            s.pop();
+            s.push(rotationMat);
+        }
+        else if(command == "translate"){
+            matrix transMat(4);
+            transMat=transMat.make_identity(4);
+            scene >> transMat.values[0][3] >> transMat.values[1][3] >>transMat.values[2][3];
+            //transMat.print();
+            transMat= transMat * s.top();
+            s.pop();
+            s.push(transMat);
+        }
+        else if(command == "scale"){
+            matrix scaleMat(4);
+            scaleMat=scaleMat.make_identity(4);
+            scene >> scaleMat.values[0][0] >> scaleMat.values[1][1] >>scaleMat.values[2][2];
+            //scaleMat.print();
+            scaleMat= scaleMat * s.top();
+            s.pop();
+            s.push(scaleMat);
+
+        }
+        else if(command == "end"){
+            break;
+        }
+
+    }
     scene.close();
     stage1.close();
 
@@ -428,7 +559,7 @@ int main()
     cout << std::setprecision(4);
 
     stage1();
-    //stage2();
+    stage2();
     //stage3();
     //scan_convert();
 
